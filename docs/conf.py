ac6d59e0b99588fa82654fed30c7595cc94ed91e
Gabriel Carballude <gabrielcarballude@gmail.com>
2018-03-20 18:57:16 -0300

    Modificando la pagina de Github para visualizar en ReadTheDocs mediante un Webhook

--- docs/conf.py
+++ docs/conf.py
@@ -86,9 +86,13 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-def setup(app):
-   app.add_javascript("custom.js")
-   app.add_stylesheet("custom.css")
+
+html_context = {
+    'css_files': [
+        '_static/theme_overrides.css',  # override wide tables in RTD theme
+        ],
+     }
+   
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
