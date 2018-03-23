72126de85d3311c3da1c890a4a95c58e18216d04
Gabriel Carballude <gabrielcarballude@gmail.com>
2018-03-23 08:39:50 -0300

    Actualizando ReadTheDocs

--- docs/conf.py
+++ docs/conf.py
@@ -89,9 +89,6 @@ html_theme_options = {
 html_static_path = ['_static']
 
 html_context = {
-    'css_files': [
-        '_static/theme_overrides.css'  # override wide tables in RTD theme
-        ]
      }
 
 # Custom sidebar templates, must be a dictionary that maps document names
